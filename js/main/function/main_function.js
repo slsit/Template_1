e91a948f8de1c1ed19d28e39059a5127e080a848
radkevich.t.s <radkevich.t.s@gmail.com>
2017-05-25 15:46:33 +0300

    new mews div

--- js/main/function/main_function.js
+++ js/main/function/main_function.js
@@ -8,13 +8,16 @@ function setSizeNewsBlock () {
     $(".news-header").css("width", document.documentElement.clientWidth*0.278);
     $(".news-header").css("height", document.documentElement.clientWidth*0.2);
 
+    $(".carousel-inner").css("width", document.documentElement.clientWidth*0.278);
+    $(".carousel-inner").css("height", document.documentElement.clientWidth*0.2);
+
     $(".main-block-header").css("width", document.documentElement.clientWidth);
     $(".main-block-header").css("height", (document.documentElement.clientWidth*4)/9);
 
-    $(".slider-header-news").css("width", (document.documentElement.clientWidth*311)/960);
+/*    $(".slider-header-news").css("width", (document.documentElement.clientWidth*311)/960);
     $(".slider-header-news").css("padding-top", (document.documentElement.clientWidth*5)/64);
     $(".slider-header-news").css("padding-left", (document.documentElement.clientWidth*73)/1920);
-    $(".slider-header-news").css("max-heightt", (document.documentElement.clientWidth*74)/192);
+    $(".slider-header-news").css("max-heightt", (document.documentElement.clientWidth*74)/192);*/
     /*$(".news-header").css("width", document.documentElement.clientWidth*0.278);
      $(".news-header").css("height", document.documentElement.clientHeight*0.68);*/
 }
